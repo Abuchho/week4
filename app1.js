97ad83581f10325c3d76f27f00050a731493e678
Andrew Buchholz <learningemail33@gmail.com>
2021-06-09 13:18:47 -0400

    hw3_Week4

---
+++ app1.js
@@ -0,0 +1,55 @@
+console.log("hello world");
+
+console.warn("This is a warning");
+
+console.error("Error");
+
+var var1;
+
+var1= "hi";
+
+console.log(var1);
+
+console.log("\n");
+
+var var2= 3;
+
+var var3= var1+var2;
+
+console.log(var3);
+
+
+// output will be hi3
+
+/*  boolean
+    Nan
+    undefined
+    number
+    string
+*/
+
+console.log(typeof var1);
+
+// string
+
+var var4="text";
+
+console.log(var4);
+
+var4= true;
+
+console.log(var4);
+
+var1= 5;
+
+var2="5";
+
+var2 = parseInt(var2,10);
+
+console.log(var1+var2);
+
+
+
+
+
+
